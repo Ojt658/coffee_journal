b2ad64bb9e5b9f28fc246c5e2f3186609827246b
Ollie <ojt@btinternet.com>
2025-10-07 23:37:22 +0100

    Added the database to hold the recipe information.

--- app/src/main/java/com/dev/coffeejournal/activities/RecordActivity.kt
+++ app/src/main/java/com/dev/coffeejournal/activities/RecordActivity.kt
@@ -8,11 +8,19 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.dev.coffeejournal.ux.AppBar
 import com.dev.coffeejournal.R
 import com.dev.coffeejournal.ui.theme.CoffeeJournalTheme
@@ -29,6 +37,20 @@ class RecordActivity : ComponentActivity() {
                             "Record a Brew",
                             canNavigateBack = true,
                             navigateUp = { finish() })
+                    }, floatingActionButton = {
+                        FloatingActionButton(
+                            onClick = {},
+                            containerColor = MaterialTheme.colorScheme.primaryContainer,
+                            contentColor = MaterialTheme.colorScheme.primary,
+                            shape = CircleShape,
+                            modifier = Modifier.padding(16.dp),
+                            content = {
+                                Icon(
+                                    imageVector = Icons.Filled.Add,
+                                    contentDescription = "Add Button"
+                                )
+                            }
+                        )
                     }
                 ) { innerPadding ->
                     RecordPage(Modifier.padding(innerPadding))
@@ -38,8 +60,11 @@ class RecordActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RecordPage(modifier: Modifier = Modifier) {
+
+
     Box (modifier = modifier.fillMaxSize()) {
         Image (
             painter = painterResource(id = R.drawable.coffee_beans_background),
